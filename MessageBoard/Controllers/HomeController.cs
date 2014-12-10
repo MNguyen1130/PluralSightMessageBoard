55d363f184243283846a259ea51e4edfc8e0648e
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-10 14:16:27 -0500

    Initial Commit

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+                
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From: {1}{0}Website: {2}{0}Message{3}{0}", Environment.NewLine, model.Name, model.Website, model.Message);
+            if(_mail.SendMail(model.Email, "noreply@yourdomain.com", "Website Contact", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+
+    }
+}
