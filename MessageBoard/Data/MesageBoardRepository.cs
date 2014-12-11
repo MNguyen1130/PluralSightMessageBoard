c074b18bea9202544de10c118bba0df865611062
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-11 15:52:25 -0500

    Finished Section 3
    Got through section 3 and part of 4

---
+++ MessageBoard/Data/MesageBoardRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageBoard.Data
+{
+    public class MesageBoardRepository : IMessageBoardRepository
+    {
+        public IQueryable<Topic> GetTopics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Reply> GetRepliesByTopic(int topicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
