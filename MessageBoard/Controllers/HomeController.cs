498bd05a822a482f6cb4f2ba0580668c6316ca02
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-12 14:28:00 -0500

    Fleshed out more stuff, need to reclone

--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -5,21 +5,25 @@ using System.Web;
 using System.Web.Mvc;
 using MessageBoard.Models;
 using MessageBoard.Services;
+using MessageBoard.Data;
 
 namespace MessageBoard.Controllers
 {
     public class HomeController : Controller
     {
         private IMailService _mail;
+        private IMessageBoardRepository _repo;
 
-        public HomeController(IMailService mail)
+        public HomeController(IMailService mail, IMessageBoardRepository repo)
         {
             _mail = mail;
+            _repo = repo;
         }
 
         public ActionResult Index()
         {
-            return View();
+            var topics = _repo.GetTopics().OrderByDescending(t => t.Created).Take(25).ToList();
+             return View(topics);
         }
 
         public ActionResult About()
--- MessageBoard/Data/Reply.cs
+++ MessageBoard/Data/Reply.cs
@@ -8,7 +8,7 @@ namespace MessageBoard.Data
     public class Reply
     {
         public int Id { get; set; }
-        public string body { get; set; }
+        public string Body { get; set; }
         public DateTime Created { get; set; }
 
         public int TopicId { get; set; }
