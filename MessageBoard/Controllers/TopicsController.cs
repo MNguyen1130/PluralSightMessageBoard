3a576f5db8b622bcce62d4be1bc7c8dc44919b9d
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-17 14:05:19 -0500

    Completed Module 5
    Finished Module 5 and started Module 6. All the AngularJS stuff was
    added in.

--- MessageBoard/Controllers/TopicsController.cs
+++ MessageBoard/Controllers/TopicsController.cs
@@ -17,9 +17,36 @@ namespace MessageBoard.Controllers
             _repo = repo;
         }
 
-        public IEnumerable<Topic> Get()
+        public IEnumerable<Topic> Get(bool includeReplies = false)
         {
-            return _repo.GetTopics().OrderByDescending(t => t.Created).Take(50).ToList();
+
+            IQueryable<Topic> results;
+
+            if (includeReplies)
+            {
+                results = _repo.GetTopicsIncludingReplies();
+            }
+            else
+            {
+                results = _repo.GetTopics();
+            }
+
+            return results.OrderByDescending(t => t.Created).Take(50).ToList();
+        }
+
+        public HttpResponseMessage Post([FromBody]Topic newTopic)
+        {
+            if (newTopic.Created == default(DateTime))
+            {
+                newTopic.Created = DateTime.UtcNow;
+            }
+
+            if (_repo.AddTopic(newTopic) && _repo.Save())
+            {
+                return Request.CreateResponse(HttpStatusCode.Created, newTopic);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
         }
     }
 }
