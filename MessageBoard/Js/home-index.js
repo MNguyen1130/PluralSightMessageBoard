6c1ce9ed1b160707c2ad1b94701c82b5f134ab8f
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-18 10:04:35 -0500

    Starting Module 6
    Adding in AngularJS. Note we have to do look up stuff a lot due to the
    changes in 1.3.x

---
+++ MessageBoard/Js/home-index.js
@@ -0,0 +1,16 @@
+﻿// home-index.js
+angular.module("myApp", []).controller("HomeIndexController", function HomeIndexController ($scope, $http) {
+    $scope.dataCount = 0;
+    $scope.data = [];
+
+    $http.get("/api/v1/topics?includeReplies=true")
+        .then(
+        function (result) {
+            // Successful
+            angular.copy(result.data, $scope.data);
+        },
+        function () {
+            // Error
+            alert("Could not load topics");
+        });
+});
