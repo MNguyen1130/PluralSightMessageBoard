f569096b288e501d5e06089fc81cf0241486fc8a
MNguyen1130 <matthewpnguyen@gmail.com>
2014-12-18 19:57:51 -0500

    Completed Module 6; Started Module 7
    Currently on AngularJS Validation

--- MessageBoard/Js/home-index.js
+++ MessageBoard/Js/home-index.js
@@ -1,16 +1,40 @@
 ﻿// home-index.js
-angular.module("myApp", []).controller("HomeIndexController", function HomeIndexController ($scope, $http) {
-    $scope.dataCount = 0;
+var module = angular.module("homeIndex", ['ngRoute']);//.controller("topicsController", function topicsController($scope, $http) {
+
+module.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        controller: "TopicsController",
+        templateUrl: "/Templates/TopicsView.html"
+    });
+
+    $routeProvider.when("/newmessage", {
+        controller: "newTopicController",
+        templateUrl: "/Templates/NewTopicView.html"
+    })
+
+    $routeProvider.otherwise({ redirectTo: "/" });
+});
+
+
+module.controller("TopicsController", function ($scope, $http) {
+
     $scope.data = [];
+    $scope.isBusy = true;
 
     $http.get("/api/v1/topics?includeReplies=true")
-        .then(
-        function (result) {
+        .then(function (result) {
             // Successful
             angular.copy(result.data, $scope.data);
         },
         function () {
             // Error
             alert("Could not load topics");
+        })
+        .then(function () {
+            $scope.isBusy = false;
         });
+});
+
+module.controller("NewTopicController", function ($scope, $http, $window) {
+
 });
